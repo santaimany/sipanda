3ba957962e7af15453437eb8cbda45c427582dd8
santaimany <santaimany@gmail.com>
2024-11-19 19:58:13 +0700

    Membuat controller untuk dashboard kades dan menambahkan model pangan

---
+++ app/Models/Pangan.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pangan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'desa_id', 'jenis_pangan', 'berat', 'tanggal'
+    ];
+
+    public function desa()
+    {
+        return $this->belongsTo(Desa::class, 'desa_id');
+    }
+}
