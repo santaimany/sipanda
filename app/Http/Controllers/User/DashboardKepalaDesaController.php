8942cdedfae0447ed58528dc24f771724c97d86d
santaimany <santaimany@gmail.com>
2024-11-19 20:28:13 +0700

    get user kepala desa -> done

--- app/Http/Controllers/User/DashboardKepalaDesaController.php
+++ app/Http/Controllers/User/DashboardKepalaDesaController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardKepalaDesaController extends Controller
 {
-    //
+    public function getUserKades()
+    {
+        $user = Auth::user();
+
+        // Pastikan user memiliki role 'kepala_desa'
+        if ($user->role !== 'kepala_desa') {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        // Ambil data nama user dan nama desa
+        $data = [
+            'name' => $user->name,
+            'desa' => $user->desa ? $user->desa->nama : 'Desa tidak ditemukan'
+        ];
+
+        return response()->json([
+            'message' => 'Success',
+            'data' => $data,
+        ], 200);
+    }
 }
