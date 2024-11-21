77ab38875c08ee3161e11ff2d0b33e6111ec2554
santaimany <santaimany@gmail.com>
2024-11-21 19:50:22 +0700

    commit changes

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
  
 // Load the Laravel application
+<<<<<<< HEAD
 require __DIR__ . '/../public/index.php';
+=======
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+ 
+// Run the application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
+>>>>>>> cccfcb1 (register FIX BANGET, penambahan config vercel, dan mengganti env menjadi database server)
