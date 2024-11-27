f0dfcde650592ee60c293330f63fb2757101b8dc
santaimany <santaimany@gmail.com>
2024-11-28 00:04:07 +0700

    melakukan penambahan fitur check status akun user serta memberikan qrcode pada user, dan menambahkan edit profile

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\Models\User;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,6 +15,7 @@ class UserController extends Controller
 
         // Cari user berdasarkan license key
         $user = User::where('license_key', $licenseKey)->first();
+
 
         if (!$user) {
             return response()->json(['message' => 'Invalid QR Code.'], 400);
@@ -27,5 +29,57 @@ class UserController extends Controller
         $user->update(['status' => 'verified']);
 
         return response()->json(['message' => 'User verified successfully.']);
+    }
+
+    
+    public function checkStatus(Request $request)
+    {
+        // Validasi input
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Cari user berdasarkan email
+        $user = User::where('email', $request->email)->first();
+
+        // Periksa apakah pengguna ada dan password cocok
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Email atau password salah.'], 401);
+        }
+
+        // Cek status akun
+        switch ($user->status) {
+            case 'verified':
+                return response()->json([
+                    'status' => 'approved',
+                    'message' => 'Akun Anda telah disetujui.',
+                ], 200);
+
+            case 'pending_qr':
+                return response()->json([
+                    'status' => 'pending_qr',
+                    'message' => 'Akun Anda telah disetujui! Lakukan Verifikasi dengan Qr Code.',
+                    'qr_code_url' => asset("storage/{$user->qr_code}"),
+                ], 200);
+
+            case 'pending':
+                return response()->json([
+                    'status' => 'pending',
+                    'message' => 'Akun anda sedang menunggu persetujuan admin.',
+                ], 200);
+
+            case 'rejected':
+                return response()->json([
+                    'status' => 'rejected',
+                    'message' => 'Akun Anda telah ditolak oleh admin.',
+                ], 200);
+
+            default:
+                return response()->json([
+                    'status' => 'unknown',
+                    'message' => 'Status akun Anda tidak diketahui.',
+                ], 400);
+        }
     }
 }
