8fe3957b5d4994f1d9bf6d786712ea452d2ef662
Syachint <syachint02@gmail.com>
2024-12-04 01:07:18 +0700

    qr testing16

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -60,7 +60,7 @@ class UserController extends Controller
                 return response()->json([
                     'status' => 'pending_qr',
                     'message' => 'Akun Anda telah disetujui! Lakukan Verifikasi dengan Qr Code.',
-                    'qr_code_url' => url("/api/qr_code/{$user->qr_code}.png"),
+                    'qr_code_url' => url("/api/qr_code/{$user->license_key}.png"),
                 ], 200);
 
             case 'pending':
